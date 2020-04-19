93eb89dc25b6ed556d260f26343f183ae85455e5
LEVIEUGE Christian <christian.levieuge@stordata.fr>
2020-04-19 12:27:22 +0200

    20200419_CLE_nodup - hashing the latest torrent name and compare to env variable. If different change and proceed to mail, other wise bye bye

--- src/torrents.js
+++ src/torrents.js
@@ -4,6 +4,7 @@ const axios = require('axios'),
       _ = require('lodash');
 
 module.exports = function fetchTorrents(limit) {
+  var md5 = require('md5');
   function buildSearchParams() {
     const params = new URLSearchParams();
 
@@ -24,5 +25,11 @@ module.exports = function fetchTorrents(limit) {
     .then(_.values)
     .then((torrents) => _.sortBy(torrents, _.last)) // Last item is the custom "addtime"
     .then(_.reverse)
-    .then((torrents) => _.slice(torrents, 0, limit));
+    .then((torrents) => _.slice(torrents, 0, limit))
+    .then((torrents) => {
+      if(md5(torrents[0][4]) === process.env.RUTORRENT_LASTHASH)
+        exit;
+      else
+        process.env.RUTORRENT_LASTHASH = md5(torrents[0][4])
+    )} 
 };
